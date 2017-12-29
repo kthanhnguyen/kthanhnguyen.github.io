c66ab24683cefe6222a6c6db9108978e957eb3d5
ThanhNguyen <nguyenkimthanh901@gmail.com>
2017-12-29 14:52:33 +0700

    upload demo 9

---
+++ trainning_web/demo_9/js/scoll.js
@@ -0,0 +1,19 @@
+$(function() {
+	    var topBtn = $('#pageTop');   
+	    //topBtn.hide();
+	    //ƒXƒNƒ[ƒ‹‚ª100‚É’B‚µ‚½‚çƒ{ƒ^ƒ“•\Ž¦
+	    $(window).scroll(function () {
+	        if ($(this).scrollTop() > 100) {
+	            topBtn.fadeIn();
+	        } else {
+	            //topBtn.fadeOut();
+	        }
+	    });
+	    //ƒXƒNƒ[ƒ‹‚µ‚Äƒgƒbƒv
+	    topBtn.click(function () {
+	        $('body,html').animate({
+	            scrollTop: 0
+	        }, 500);
+	        return false;
+	    });
+});
